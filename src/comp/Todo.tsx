6e44394f7f9a3f298c22a53daf5a463e6ad4afa7
baluworks <baluworks@gmail.com>
2020-09-13 18:52:37 +0530

    onDelete functionality for item passing arguments to eventHandlers.

--- src/comp/Todo.tsx
+++ src/comp/Todo.tsx
@@ -1,40 +1,46 @@
-import React  from 'react';
-import {TodoList} from './TodoList';
-import {Item,State} from './todo.model'
+import React from "react";
+import { TodoList } from "./TodoList";
+import { Item, State } from "./todo.model";
 
-export class TODOComp extends React.Component<any,State>{
-  
-
-constructor(props: any){
-  super(props);
-  this.onHandleSubmit = this.onHandleSubmit.bind(this);
-  this.handleChange = this.handleChange.bind(this);
-  this.onHandleDelete = this.onHandleDelete.bind(this);
-  this.state = {items:[],currentText:""}
-}
-onHandleSubmit(e:any){
-  e.preventDefault();
-  this.setState((prevState)=>{
-   const newItem:Item = {id:Date.now(),text:prevState.currentText}
-    return {items:[...prevState.items,newItem],currentText:""};
-  })
-}
-handleChange(e:any){
-this.setState({currentText:e.target.value})
-}
-onHandleDelete(e:any){
-// console.log("one",e.target.key);
-}
-  render(){
-    return(
+export class TODOComp extends React.Component<any, State> {
+  constructor(props: any) {
+    super(props);
+    this.onHandleSubmit = this.onHandleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.onHandleDelete = this.onHandleDelete.bind(this);
+    this.state = { items: [], currentText: "" };
+  }
+  onHandleSubmit(e: any) {
+    e.preventDefault();
+    this.setState((prevState) => {
+      const newItem: Item = { id: Date.now(), text: prevState.currentText };
+      return { items: [...prevState.items, newItem], currentText: "" };
+    });
+  }
+  handleChange(e: any) {
+    this.setState({ currentText: e.target.value });
+  }
+  onHandleDelete(e: any, id: number) {
+    console.log("one", id);
+  }
+  render() {
+    return (
       <div>
-        <TodoList items = {this.state.items} onDelete = {this.onHandleDelete}/>
-        <form >
+        <TodoList items={this.state.items} onDelete={this.onHandleDelete} />
+        <form>
           <label htmlFor="todo_text_box">Item to be done?</label>
-          <input type="text" name="todo_text_box" id="todo_text_box" value = {this.state.currentText} onChange = {this.handleChange}/>
-          <button onClick = {this.onHandleSubmit}>Add {this.state.items.length} #Item</button>
+          <input
+            type="text"
+            name="todo_text_box"
+            id="todo_text_box"
+            value={this.state.currentText}
+            onChange={this.handleChange}
+          />
+          <button onClick={this.onHandleSubmit}>
+            Add {this.state.items.length} #Item
+          </button>
         </form>
       </div>
-    )
+    );
   }
 }
--- src/comp/TodoList.tsx
+++ src/comp/TodoList.tsx
@@ -1,23 +1,32 @@
+import React from "react";
+import { Item } from "./todo.model";
 
-import  React  from 'react';
-import {Item} from './todo.model'
-
-interface ItemModel{
-  items :Array<Item>,
-  
+interface ItemModel {
+  items: Array<Item>;
 }
-export class TodoList extends React.Component<any,ItemModel>{
-  constructor(props:ItemModel){
+export class TodoList extends React.Component<any, ItemModel> {
+  constructor(props: ItemModel) {
     super(props);
   }
-render(){
- 
-return (<div>
-  <ul>
-    {this.props.items.map((item: Item)=>
-  <li key={item.id}>{item.id}-{item.text} <button  key={item.id} onClick={this.props.onDelete}>del</button></li> 
-    )}
-  </ul>
-</div>)
-}
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.props.items.map((item: Item) => (
+            <li key={item.id}>
+              {item.id}-{item.text}{" "}
+              <button
+                key={item.id}
+                onClick={(e) => {
+                  this.props.onDelete(e, item.id);
+                }}
+              >
+                del
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
